91eb5442dd5d8f22bd6e364305b3dcfd09ccb099
sarbal <sarahballouz@gmail.com>
2017-12-05 14:53:58 -0500

    test

--- bin/de_aligner_analysis.r
+++ bin/de_aligner_analysis.r
@@ -1,22 +1,31 @@
+# Load gene sets and attributes
 load("gene_annotations_v22.Rdata")
 load("DE_sex_diff_genesets.Rdata")
-load("sample_counts.Rdata")
+
+# Helper code and functions
 source("de_aligner_helper.r")
 require(EGAD)
 
 
 # samples
-# geuvadis data set
+# load geuvadis data set at default parameter 0.66
+load("sample_counts.Rdata")
+series = c("0.66")  # change if multiple parameters tested
 
 group = as.numeric(sex)
-n_rep = 10       # number of repeats
+nS = length(samples)
+n_rep = 10    # number of repeats
 n_sub = 200   # number of samples to sub sample to
 
+# Filter for genes with entrezIDs and non zeroes
+f.k = !is.na(attr$entrezID) & rowSums(counts_exp) > 0
+
+
 deg.sub = list()
-for( i in 1:length(series)){
-	X = exp_counts[[i]]
+# for( i in 1:length(series)){
+	X = counts_exp
 	rownames(X) = attr$ensemblID
-	colnames(X) = samples 
+	colnames(X) = samples
 	N = colSums(X,na.rm=T)
 	# convert counts to CPM
 	X.cpm = sapply(1:nS, function(k) 10^6*X[,k]/N[k] )
@@ -38,14 +47,15 @@ for( i in 1:length(series)){
 			deg.sub[[i]][[j]]= list()
 	        	deg.sub[[i]][[j]]$de = de
 		}
-}
+# }
 
 save(deg.sub, file="deg.sub.wilcox.Rdata")
 
 # random DE (shuffle samples)
 deg.sub.rand = list()
-for( i in 1:length(series)){
-	X = exp_counts[[i]]
+i = 1
+# for( i in 1:length(series)){
+	X = counts_exp
 	rownames(X) = attr$ensemblID
 	colnames(X) = samples
 	N = colSums(X,na.rm=T)
@@ -79,15 +89,13 @@ for( i in 1:length(series)){
 
         save(deg.sub.rand.sub,i, file=paste0(i,"deg.sub.rand.wilcox.Rdata") )
 
-}
+# }
 
 save(deg.sub.rand, file="deg.sub.rand.wilcox.Rdata")
 
 
-# Filter for genes with DE signal
-f.k = 
 
-# Calculate enrichments
+# Calculate enrichments, sub.labels is in the DE_sex_diff file 
 rocs.all.r = list()
 for( i in 1:length(series) ) {
         deg.adj = deg.sub.rand[[i]][[1]][[1]]
